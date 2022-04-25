aa440221ca9a3d7584ecd7b4acd31799f784c962
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2022-04-25 16:38:02 +0100

    add single outlier and business tracking API

--- mlting/mlting.py
+++ mlting/mlting.py
@@ -4,16 +4,34 @@ import requests
 
 
 def save_test_to_json(
-    test: str, description: str, prediction: str, df: pd.DataFrame
+    test: str, description: str, prediction: str, df: pd.DataFrame, tracking=False
 ) -> None:
     test_json = json.dumps(
         {
             "test": {"name": test, "description": description},
             "prediction": prediction,
-            "features": df.to_json(orient="split"),
+            "features": df.to_dict(),
         }
     )
+    if tracking:
+        url = "http://localhost:5000"
+        headers = {"Content-type": "application/json"}
+        requests.post(url + "/tests/v1/add", data=test_json, headers=headers)
+    else:
+        with open("assets/data/mlting_test.json", "w") as file:
+            file.write(test_json)
 
-    url = "http://localhost:5000"
-    headers = {"Content-type": "application/json"}
-    requests.post(url + "/tests/v1/add", data=test_json, headers=headers)
+
+def test_single_outlier(prediction, desired_output):
+    if prediction == desired_output:
+        return True
+    else:
+        return False
+
+
+def get_business_test_data(tracking=False):
+    if tracking:
+        raise Exception("to be replaced with read from test tracking API")
+    else:
+        with open("../assets/data/mlting_test.json", "r") as file:
+            return pd.DataFrame(json.load(file).get("features"))
