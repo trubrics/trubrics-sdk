4f98b50bc519fdb4521df1609cef69e59644ff69
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2022-05-13 16:02:30 +0200

    add performance test

---
+++ mlting/mltests/performance.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+import pandas as pd
+
+from mlting.base import BaseModel, BaseTester
+
+
+def test_performance_against_threshold(
+    model: Callable,
+    test_data: pd.DataFrame,
+    evaluation_function: Callable,
+    target: str,
+    runner: str,
+    threshold: float = 0.8,
+):
+    """
+    Compares performance of a model on a dataset to a hard coded threshold value.
+    """
+    model = BaseModel(model)
+
+    if evaluation_function.__name__ == "accuracy_score":
+        type = "greater"
+    else:
+        NotImplementedError("The evaluation type is not recognized.")
+    predictions = model.predict(test_data)
+    result = evaluation_function(test_data[target], predictions)
+    BaseTester(result, threshold).assertion(type=type, runner=runner)
