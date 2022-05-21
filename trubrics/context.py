15ddb723d9387972a84c98424f9f9497579f247e
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2022-05-21 15:57:33 +0200

    add pandas data validation for data context

--- trubrics/context.py
+++ trubrics/context.py
@@ -1,7 +1,8 @@
-from typing import Callable, Optional, Union
+from typing import Any, Callable, List, Optional, Union
 
 import pandas as pd
-from pydantic import BaseModel
+from jsonschema import SchemaError
+from pydantic import BaseModel, validator
 from sklearn.base import BaseEstimator
 
 
@@ -16,6 +17,7 @@ class ModelContext(BaseModel):
     class Config:
         allow_mutation = False
         arbitrary_types_allowed = True
+        extra = "forbid"
 
 
 class DataContext(BaseModel):
@@ -24,8 +26,40 @@ class DataContext(BaseModel):
     name: Optional[str] = None
     training_data: Optional[pd.DataFrame] = None
     testing_data: pd.DataFrame
-    target_col: str
+    categorical_columns: Optional[List[str]] = None
+    target_column: str
 
     class Config:
         allow_mutation = False
         arbitrary_types_allowed = True
+        extra = "forbid"
+
+    @validator("testing_data")
+    def testing_and_training_must_have_same_schema(cls, v: pd.DataFrame, values: List[Any]):
+        if values["training_data"] is not None and not _schema_is_equal(v, values["training_data"]):  # type: ignore
+            raise SchemaError("Testing and training data must have identical schemas.")
+        return v
+
+    @validator("target_column")
+    def target_column_must_be_in_data(cls, v: str, values: List[Any]):
+        if v not in values["testing_data"].columns:  # type: ignore
+            raise KeyError("Target column must be in testing_data column names.")
+        return v
+
+    @validator("categorical_columns")
+    def categorical_columns_must_be_in_data(cls, v: str, values: List[Any]):
+        if not set(v).issubset(values["testing_data"].columns):  # type: ignore
+            raise KeyError("All categorical columns must be in testing_data column names.")
+        return v
+
+    @validator("target_column")
+    def target_column_must_not_be_in_categoricals(cls, v: str, values: List[Any]):
+        if values["categorical_columns"] is not None and v in values["categorical_columns"]:  # type: ignore
+            raise Exception(
+                "Target column should not feature as a categorical column. Categorical columns only refer to features."
+            )
+        return v
+
+
+def _schema_is_equal(df1: pd.DataFrame, df2: pd.DataFrame) -> bool:
+    return df1.dtypes.equals(df2.dtypes)
