f08115bac447f8abcfa53c7b67a8122e2eee8d90
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2022-07-28 16:11:44 +0200

    replace BaseException with Exception

--- trubrics/exceptions.py
+++ trubrics/exceptions.py
@@ -1,18 +1,18 @@
-class ValidationOutputError(BaseException):
+class ValidationOutputError(Exception):
     """An exception signalling that the output from a validation function is invalid."""
 
 
-class PandasSchemaError(BaseException):
+class PandasSchemaError(Exception):
     """An exception signalling that two DataFrames do not have the same schema (column names & types)."""
 
 
-class ClassifierNotSupportedError(BaseException):
+class ClassifierNotSupportedError(Exception):
     """An exception signalling that the output from a validation function is invalid."""
 
 
-class UnknownEstimatorType(BaseException):
+class UnknownEstimatorType(Exception):
     """An exception signalling that an estimator type is not recognised."""
 
 
-class UnknownValidationError(BaseException):
+class UnknownValidationError(Exception):
     """An exception signalling that validation is not recognised by a given validator."""
