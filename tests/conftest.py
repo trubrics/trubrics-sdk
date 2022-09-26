d660d90a07051f93d69138c6670b377335b5307f
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2022-09-26 19:12:59 +0200

    add test cases for performance threshold

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import joblib
 import pandas as pd
 import pytest
 
+from examples.cli.custom_scorer import custom_scorers
+from examples.cli.slicing_functions import slicing_functions
 from trubrics.context import DataContext, TrubricContext
 from trubrics.validations import ModelValidator
 
@@ -36,7 +38,9 @@ def classifier_model():
 
 @pytest.fixture
 def validator_classifier(data_context, classifier_model):
-    return ModelValidator(data=data_context, model=classifier_model)
+    return ModelValidator(
+        data=data_context, model=classifier_model, custom_scorers=custom_scorers, slicing_functions=slicing_functions
+    )
 
 
 @pytest.fixture
--- tests/test_validations/test_validator_with_classifier.py
+++ tests/test_validations/test_validator_with_classifier.py
@@ -1,6 +1,6 @@
 import pytest
 
-from trubrics.exceptions import EstimatorTypeError
+from trubrics.exceptions import EstimatorTypeError, SklearnMetricTypeError
 
 
 def test__validate_minimum_functionality(validator_classifier):
@@ -14,10 +14,60 @@ def test__validate_minimum_functionality_in_range_raises(validator_classifier):
         validator_classifier._validate_minimum_functionality_in_range()
 
 
-def test__validate_performance_against_threshold(validator_classifier):
-    result = validator_classifier._validate_performance_against_threshold(metric="accuracy", threshold=0.7)
-    actual = False, {"performance": 0.5, "sample_size": 6}
+@pytest.mark.parametrize(
+    "kwargs,outcome,actual_result",
+    [
+        (
+            {"metric": "accuracy", "threshold": 0.7, "dataset": "testing_data", "data_slice": None},
+            False,
+            {"performance": 0.5, "sample_size": 6},
+        ),
+        (
+            {"metric": "accuracy", "threshold": 0.1, "dataset": "testing_data", "data_slice": None},
+            True,
+            {"performance": 0.5, "sample_size": 6},
+        ),
+        (
+            {"metric": "my_custom_loss", "threshold": -0.7, "dataset": "testing_data", "data_slice": None},
+            True,
+            {"performance": -0.5, "sample_size": 6},
+        ),
+        (
+            {"metric": "my_custom_loss", "threshold": -0.1, "dataset": "testing_data", "data_slice": None},
+            False,
+            {"performance": -0.5, "sample_size": 6},
+        ),
+        (
+            {"metric": "accuracy", "threshold": 0.9, "dataset": "testing_data", "data_slice": "female"},
+            False,
+            {"performance": 0.0, "sample_size": 1},
+        ),
+    ],
+)
+def test__validate_performance_against_threshold(validator_classifier, kwargs, outcome, actual_result):
+    result = validator_classifier._validate_performance_against_threshold(**kwargs)
+    actual = outcome, actual_result
     assert result == actual
+
+
+@pytest.mark.parametrize(
+    "kwargs,error",
+    [
+        ({"metric": "accuracy", "threshold": "something", "dataset": "testing_data", "data_slice": None}, TypeError),
+        (
+            {"metric": "some_metric", "threshold": 10, "dataset": "testing_data", "data_slice": None},
+            SklearnMetricTypeError,
+        ),
+        ({"metric": "accuracy", "threshold": 10, "dataset": "other_data", "data_slice": None}, ValueError),
+        (
+            {"metric": "accuracy", "threshold": 10, "dataset": "testing_data", "data_slice": "random_data_slice"},
+            ValueError,
+        ),
+    ],
+)
+def test__validate_performance_against_threshold_raises(validator_classifier, kwargs, error):
+    with pytest.raises(error):
+        validator_classifier._validate_performance_against_threshold(**kwargs)
 
 
 def test__validate_test_performance_against_dummy(validator_classifier):
