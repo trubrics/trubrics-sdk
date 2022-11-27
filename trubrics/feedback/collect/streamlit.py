7735b9a0bb57ac3cab8736763082b91f6984394b
rishabsinghh <rishabsingh240@gmail.com>
2022-11-27 20:14:26 +0530

    Add Docstrings to Feedback Components V1.

--- trubrics/feedback/collect/streamlit.py
+++ trubrics/feedback/collect/streamlit.py
@@ -18,7 +18,7 @@ def collect_feedback_streamlit(
 
     Args:
         path : The path where the feedback file gets saved. If empty, defaults to current working directory.
-        file_name: Name of the file. If Empty,defaults to "Feedback.json"
+        file_name: Name of the file. If empty,defaults to "Feedback.json"
         metadata: Any other form of metric which the user wants to log into the feedback file such as feature value,prediction,etc. If empty,defaults to None.
         tags: list of any tags for this feedback file. If empty,defaults to None.
 
