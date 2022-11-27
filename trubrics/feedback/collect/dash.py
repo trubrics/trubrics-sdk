4493d4174c3279ebd1a2e7b21022d2a51a628652
rishabsinghh <rishabsingh240@gmail.com>
2022-11-27 20:22:23 +0530

    Add Docstrings to Feedback Components V1.

--- trubrics/feedback/collect/dash.py
+++ trubrics/feedback/collect/dash.py
@@ -18,7 +18,7 @@ def collect_feedback_dash(
     Feedback can be in the form of text or any other format. If no path is given, it saves it in the default working directory.
     
     Args:
-        path : The path where the feedback file gets saved. If Empty, defaults to current working directory.
+        path : The path where the feedback file gets saved. If empty, defaults to current working directory.
         file_name: Name of the file. If Empty,defaults to "Feedback.json".
         metadata: Any other form of metric which the user wants to log into the feedback file such as feature value,prediction,etc. If empty, defaults to None.
         tags: list of any tags for this feedback file. If empty, defaults to None.
