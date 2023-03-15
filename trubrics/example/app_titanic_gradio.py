48046813a674ab29bec9c2c0b38455bfdd894550
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2023-03-15 19:12:42 +0100

    update FeedbackCollector in README

--- trubrics/example/app_titanic_gradio.py
+++ trubrics/example/app_titanic_gradio.py
@@ -5,6 +5,6 @@ from trubrics.integrations.gradio import collect_feedback
 with gr.Blocks() as demo:
     gr.Markdown("Gradio app to collect user feedback on models.")
     with gr.Tab("Feedback"):
-        collect_feedback(tags=["Gradio"], save_ui=False)  # set to True to save feedback to Trubrics
+        collect_feedback(tags=["Gradio"])
 
 demo.launch()
