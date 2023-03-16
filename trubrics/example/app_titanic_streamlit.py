31b44a8e740ae07839418d171817abc0adc91c56
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2023-03-16 14:33:28 +0100

    simplify titanic demo app

--- trubrics/example/app_titanic_streamlit.py
+++ trubrics/example/app_titanic_streamlit.py
@@ -8,7 +8,6 @@ from trubrics.example import get_titanic_data_and_model
 from trubrics.example import titanic_config as tc
 from trubrics.integrations.streamlit import (
     FeedbackCollector,
-    explore_testing_data,
     generate_what_if_streamlit,
 )
 
@@ -39,6 +38,8 @@ def init_trubrics(trubrics_platform_auth):
 def main(trubrics_platform_auth: Optional[str] = None):
     model, data_context, collector = init_trubrics(trubrics_platform_auth)
 
+    st.title("Titanic Demo App")
+
     with st.sidebar:
         if trubrics_platform_auth == "multiple_users":
             st.title("Authentication")
@@ -47,22 +48,27 @@ def main(trubrics_platform_auth: Optional[str] = None):
         df = generate_what_if_streamlit(data_context=data_context)
     wi_prediction = model.predict(df)[0]
 
-    metadata = {"what_if_data": df.to_dict(), "what_if_prediction": wi_prediction}
+    metadata = {"data": df.to_dict(), "prediction": wi_prediction}
 
-    st.title("View model prediction")
-    if wi_prediction:
-        prediction = '<p style="color:Green;">This passenger would have survived.</p>'
-    else:
-        prediction = '<p style="color:Red;">This passenger would have died.</p>'
-    st.markdown(prediction, unsafe_allow_html=True)
+    prediction = "## " + str(wi_prediction) + f"{' (survived)' if wi_prediction else  ' (did not survive)'}"
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("## Model prediction:")
+    with col2:
+        st.markdown(prediction, unsafe_allow_html=True)
+
+    st.markdown("## 3 types of feedback:")
+
+    st.markdown('##### 1 - "Does this prediction look correct?"')
     collector.st_feedback(type="thumbs", metadata=metadata, path="./thumbs_feedback.json")
+    st.markdown("***")
 
-    st.title("Report an issue")
-    collector.st_feedback(type="issue", metadata=metadata)
+    st.markdown('##### 2 - "How satisfied are you with this prediction?"')
+    collector.st_feedback(type="faces", metadata=metadata, path="./thumbs_faces.json")
+    st.markdown("***")
 
-    st.title("View data")
-    explore_testing_data(data_context=data_context, model=model)
-    collector.st_feedback(type="faces", tags=["example tag"])
+    st.markdown('##### 3 - "Provide your feedback!"')
+    collector.st_feedback(type="issue", metadata=metadata, path="./thumbs_issue.json")
 
 
 if __name__ == "__main__":
