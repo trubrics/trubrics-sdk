7af2f5c5686b860d0f6084f02b1d8ce2344251f4
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2023-03-28 11:35:25 +0200

    update streamlit tests

--- tests/test_feedback/test_feedback_streamlit.py
+++ tests/test_feedback/test_feedback_streamlit.py
@@ -17,12 +17,12 @@ def test_st_auth_raises():
 @pytest.mark.parametrize(
     "kwargs",
     [
-        ({"type": "custom"}),
-        ({"type": "random"}),
-        ({"type": "issue", "user_response": {"test": "test"}}),
-        ({"type": "issue", "open_feedback_label": "test"}),
-        ({"type": "faces", "user_response": "desc"}),
-        ({"type": "thumbs", "user_response": "desc"}),
+        ({"feedback_type": "custom"}),
+        ({"feedback_type": "random"}),
+        ({"feedback_type": "issue", "user_response": {"test": "test"}}),
+        ({"feedback_type": "issue", "open_feedback_label": "test"}),
+        ({"feedback_type": "faces", "user_response": "desc"}),
+        ({"feedback_type": "thumbs", "user_response": "desc"}),
     ],
 )
 def test_st_feedback_raises(kwargs):
