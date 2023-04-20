e8712ec5bcbf61146174de7f4806378b570d8786
jeffkayne <43336277+jeffkayne@users.noreply.github.com>
2023-04-20 19:29:25 +0200

    fix tests

--- tests/test_validations/test_run.py
+++ tests/test_validations/test_run.py
@@ -18,20 +18,24 @@ def test_run_trubric(data_context, classifier_model, trubric):
     all_validation_results = run_context.generate_validations_from_trubric()
 
     actuals = (
-        ("validate_minimum_functionality", "error", "pass"),
-        ("validate_performance_against_threshold", "experiment", "pass"),
-        ("validate_master_age", "warning", "pass"),
+        ("validate_minimum_functionality", "error", True),
+        ("validate_performance_against_threshold", "experiment", True),
+        ("validate_master_age", "warning", True),
     )
 
     for actual, result in zip(actuals, all_validation_results):
         assert result.validation_type == actual[0]
         assert result.severity == actual[1]
-        assert result.outcome == actual[2]
+        assert result.passed == actual[2]
 
 
 def test_run_trubric_raises(data_context, classifier_model):
     trubric_dict = {
         "name": "my_first_trubric",
+        "passed": False,
+        "total_passed": 0,
+        "total_failed": 1,
+        "failing_severity": "warning",
         "model_name": "my_model",
         "model_version": 0.1,
         "data_context_name": "my_first_dataset",
@@ -42,7 +46,7 @@ def test_run_trubric_raises(data_context, classifier_model):
                 "validation_type": "some_random_validation_name_that_is_not_in_a_validator",
                 "validation_kwargs": {"args": [], "kwargs": {}},
                 "explanation": "some docstring",
-                "outcome": "fail",
+                "passed": False,
                 "severity": "warning",
                 "result": {},
             }
